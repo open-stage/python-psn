5ffa5f2f373e048edec7310b06eef1253cc7f830
vanous <petr@linuks.cz>
2023-06-21 20:10:17 +0200

    update for 0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 ### Changelog
 
+#### Version 0.2.0
+* Provide default IP address
+
 #### Version 0.1.0
 * Initial release
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pypsn",
-    version="0.1.1",
+    version="0.2",
     long_description=long_description,
     description="PosiStageNet parser",
     license="MIT",
